2e6072ac331f82de8f99e4267d344ad855f51196
mkchu243 <mkchu243@gmail.com>
2013-04-16 23:04:50 -0700

    audio manager

---
+++ Server/AudioManager.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <SFML/Audio.hpp>
+#include <map>
+#define NUM_SOUNDS 5
+
+class AudioManager{
+private:
+  static std::map<std::string, sf::SoundBuffer*> soundBuffers;
+  static std::map<std::string, std::string> musics;
+  static sf::Sound sounds[NUM_SOUNDS];
+  static sf::Music music;
+
+  //load a sound into the soundBuffers with reference name key
+  //and filename sound
+  static void loadSound(std::string key, std::string sound);
+
+public:
+  //load all the sounds
+  static void loadSounds();
+
+  //play the given sound at given position
+  static void playSound(std::string key, float x, float y, float z);
+
+  //play this music
+  static void playMusic(std::string music);
+
+};
