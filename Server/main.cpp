c9b6fdd2a6c4f221587011b09d8fd078a4a5b2ba
mkchu243 <mkchu243@gmail.com>
2013-05-02 11:39:23 -0700

    merged

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,11 +1,9 @@
 #include "Server.h"
 #include "Client.h"
 #include "Player.h"
-#include "quadtree.h"
 #include <cctype> //this is for toupper method
 
 int main(){  
-  //Quadtree::test();
   std::cout << "Run (s)erver or (c)lient?: ";
   std::string input = ""; 
   std::getline(std::cin, input);
