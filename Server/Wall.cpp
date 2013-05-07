ca73d50993fd1a709243edae05d545a3dcca7fe9
Allen <zhen.xiao10@gmail.com>
2013-05-07 14:08:10 -0700

    adding bounding boxes for wall

--- Server/Wall.cpp
+++ Server/Wall.cpp
@@ -1,7 +1,8 @@
 #include "Wall.h"
 
+const float Wall::sphereRadius = 5.0f;
 
-Wall::Wall(unsigned int w, unsigned int d, unsigned int h, Coordinate& sc, Direction direct):centerPositions()
+Wall::Wall(unsigned int w, unsigned int d, unsigned int h, Coordinate& sc, Direction direct, Map * m):centerPositions()
 {
   width = w;
   depth = d;
@@ -10,6 +11,14 @@ Wall::Wall(unsigned int w, unsigned int d, unsigned int h, Coordinate& sc, Direc
   currentCenter = 0;
   position = sc;
   direction = direct;
+
+  BoundingBox* b = new BoundingBox(gx::vector4(direct.x,direct.y,direct.z),gx::vector3(1,0,0),gx::vector3(0,1,0),gx::vector3(0,0,1),
+    sphereRadius,sphereRadius,sphereRadius);
+  //BoundingSphere* b = new BoundingSphere(gx::vector4(x,y,z),sphereRadius);
+  boundingObjs.push_back(b);
+  m->addToQtree(this);
+  this->map = m;
+  updateBounds();
 }
 
 
@@ -37,4 +46,9 @@ void Wall::addNewCenters(std::vector<Coordinate>& centers)
 {
   for(unsigned i = 0; i < centers.size(); i++)
     centerPositions.push_back(centers[i]);
+}
+
+void Wall::updateBounds(){
+  //update the bounding objects
+  boundingObjs[0]->setCenter(gx::vector4(position.x, position.y, position.z));
 }
