a3c319d67e21378f2b5fa20fdddc06777b85e234
Matthew <mscheife@ACS-CSEB220-16.ad.ucsd.edu>
2013-05-29 22:09:51 -0700

    Smooth animations

--- Server/StaticEnums.h
+++ Server/StaticEnums.h
@@ -1,10 +1,6 @@
 #pragma once
-//#define _USE_MATH_DEFINES
-//#include <math.h>
-#include <ctgmath>
+#include <cmath>
 #include <string>
-
-//const int NUM_PLAYERS = 2; use the config file now
 
 enum move_t {
   FORWARD = 0,
